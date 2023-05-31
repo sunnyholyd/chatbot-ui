ab2f47c06206571e0f91a04656b7736b65242752
sunhuadong01 <sunhuadong01@zhihu.com>
2023-05-31 14:49:28 +0800

    commit updated code

---
+++ pages/api/login.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+// import { UserLogic, AccessControlLogic } from "database";
+import { ResponseStatus } from "@/types/typing.d";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+    // const userLogic = new UserLogic();
+    // const accessControlLogic = new AccessControlLogic();
+
+    // if (!(await userLogic.login(email, password))) {
+      // return NextResponse.json({ status: ResponseStatus.wrongPassword });
+    // }
+
+    // const tokenGenerator = await accessControlLogic.newJWT(email);
+    // const tokenGenerator = {token: "test-token", exp: 123};
+    const tokenGenerator = null;
+
+    if (!tokenGenerator)
+      return NextResponse.json({
+        status: ResponseStatus.Failed,
+      });
+
+    const { token: sessionToken, exp } = tokenGenerator;
+    return NextResponse.json({
+      status: ResponseStatus.Success,
+      sessionToken,
+      exp,
+    });
+  } catch (error) {
+    console.error("[SERVER ERROR]", error);
+    return new Response("[INTERNAL ERROR]", { status: 500 });
+  }
+}
+
+export const runtime = "edge";
